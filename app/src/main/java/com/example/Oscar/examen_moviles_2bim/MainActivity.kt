b35d0c555f4e587f3481daea1c93f6da16d75e2d
OscarLDF <38290917+OscarLDF@users.noreply.github.com>
2018-07-15 17:16:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/Oscar/examen_moviles_2bim/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.Oscar.examen_moviles_2bim
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
